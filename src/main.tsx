d2fc60b541804d84c10bf7a28b208421be7eb4c4
JM Mac <johnmarkmancol@gmail.com>
2026-07-14 09:59:07 +0800

    chore: remove default index page

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App'
-import Editor from './pages/Editor'
-import { TooltipProvider } from './components/ui/tooltip'
-import { Toaster } from './components/ui/sonner'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Editor from "./pages/Editor";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { Toaster } from "./components/ui/sonner";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <TooltipProvider>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/editor" element={<Editor />} />
+          <Route path="/" element={<Editor />} />
         </Routes>
         <Toaster />
       </TooltipProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
